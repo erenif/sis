727e01e25677d3a5f5a4d20a7ca23bc209b338d2
Bora Baran <117600305+Bor4brn@users.noreply.github.com>
2024-12-14 00:18:47 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,32 +4,56 @@ CREATE TABLE IF NOT EXISTS Course_Table (
     quota INT NOT NULL,
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
-    syllabus VARCHAR(255) NOT NULL
+    syllabus VARCHAR(255) NOT NULL,
+    credits INT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS Prerequisite_Table (
+    course_id INT NOT NULL,
+    prerequisite_course_id INT NOT NULL,
+    PRIMARY KEY (course_id, prerequisite_course_id),
+    FOREIGN KEY (course_id) REFERENCES Course_Table(course_id),
+    FOREIGN KEY (prerequisite_course_id) REFERENCES Course_Table(course_id)
+);
+
 
 CREATE TABLE IF NOT EXISTS Student_Table (
     student_id INT PRIMARY KEY,
-    student_name VARCHAR(255) NOT NULL
+    student_name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    gpa FLOAT DEFAULT 0.0,
+    available_credits INT NOT NULL 
 );
 
 CREATE TABLE IF NOT EXISTS Professor_Table (
     professor_id INT PRIMARY KEY,
-    professor_name VARCHAR(255) NOT NULL,
+    professor_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Admin_Table(
     admin_id INT PRIMARY KEY,
-    admin_name VARCHAR(255) NOT NULL
+    admin_name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Enrollment_Table (
     student_id INT,
     course_id INT,
-    grade INT,
+    grade VARCHAR(2),
     PRIMARY KEY (student_id, course_id),
     FOREIGN KEY (student_id) REFERENCES Student_Table(student_id),
     FOREIGN KEY (course_id) REFERENCES Course_Table(course_id)
 );
+
+CREATE TABLE IF NOT EXISTS Schedule_Table (
+    course_id INT NOT NULL,
+    day_of_week VARCHAR(10) NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    PRIMARY KEY (course_id, day_of_week),
+    FOREIGN KEY (course_id) REFERENCES Course_Table(course_id) 
+);
+
 
 CREATE TABLE IF NOT EXISTS Teaching_Table (
     professor_id INT,
